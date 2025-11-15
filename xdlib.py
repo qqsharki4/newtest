8c8a4cd73c1b401da9410e9983b5a9a6f27a7c80
qqsharki4 <159995328+qqsharki4@users.noreply.github.com>
2025-11-15 16:43:09 +0700

    Update xdlib.py

--- xdlib.py
+++ xdlib.py
@@ -1,6 +1,6 @@
 # This file is part of XDesai Mods.
 # I made this library to share various utility functions across my modules.
-# You can use this library in your own modules as well.yhrtnrtn
+# You can use this library in your own modules as well.yhrtnrtnкер
 
 # P.S this library is still under development and may receive updates in the future.ро
 
