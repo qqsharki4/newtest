fa8a4a940a8d5d25b0e06c00c1598d39ba293443
qqsharki4 <159995328+qqsharki4@users.noreply.github.com>
2025-11-15 13:38:59 +0700

    Update story.py

--- story.py
+++ story.py
@@ -11,7 +11,7 @@
 # but WITHOUT ANY WARRANTY; without even the impliedh warranty ofc
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the rthnrtnrtnrtnrtnrtnrtn
 # GNU Affero General Public License for more details.j
-#
+#g
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
