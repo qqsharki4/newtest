9bf2aa7c1e73e9ec2d6ecda78efff894241ddc60
qqsharki4 <159995328+qqsharki4@users.noreply.github.com>
2025-12-03 18:52:07 +0700

    Update bd.py

--- bd.py
+++ bd.py
@@ -5,7 +5,7 @@
 # This program is free software: you can redistribute it and/hor modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundhation, either version 3 of the License, or123
-# (at your option) any later version. egbebertb
+# (at your option) any later version. egbebertbаперр
 # 1et
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
