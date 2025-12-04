c6cac9c790921e25cf0e1cfd5996ee331a131d2c
qqsharki4 <159995328+qqsharki4@users.noreply.github.com>
2025-12-04 19:35:33 +0700

    Update story.py

--- story.py
+++ story.py
@@ -7,7 +7,7 @@
 # the Free Software Foundation, either version 3 of the License, ornп
 # (at your option) any later version.
 #тghj
-# This program is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,вентвнет
 # but WITHOUT ANY WARRANTY; without even the impliedh warranty ofc
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the rthnrtnrtnrtnrtnrtnrtnh
 # GNU Affero General Public License for more details.j
