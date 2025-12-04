ca80e3407b9e4531285d327df63fbf7eaef31379
qqsharki4 <159995328+qqsharki4@users.noreply.github.com>
2025-12-04 21:36:23 +0700

    Update au.py

--- au.py
+++ au.py
@@ -9,7 +9,7 @@
 # 12
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See theвпар 
+# MERCHfghANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See theвпар 
 # GNU Affero General Public Licensedfg for more detailgs.
 # 
 # You should have received a copy of the GNU Affero General Public License
