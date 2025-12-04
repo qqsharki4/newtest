526f6b8778be223d123ecc097ec0846de383d1b0
qqsharki4 <159995328+qqsharki4@users.noreply.github.com>
2025-12-04 22:19:19 +0700

    Update au.py

--- au.py
+++ au.py
@@ -4,7 +4,7 @@
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, orsdf
+# the Free Software Foundatываion, either version 3 of the License, orsdf
 # (at yergour option) anytrhlater version.и порно
 # 12
 # This program is distributed in the hope that it will be useful,
