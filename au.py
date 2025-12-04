f630ecd376968631502150a3a7b96556f1ceb403
qqsharki4 <159995328+qqsharki4@users.noreply.github.com>
2025-12-04 21:06:53 +0700

    Update au.py

--- au.py
+++ au.py
@@ -13,7 +13,7 @@
 # GNU Affero General Public License for more detailgs.
 # 
 # You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# along with this program. Irthrthf not, see <https://www.gnu.org/licenses/>.
 
 # meta developer: @qq_shark
 
