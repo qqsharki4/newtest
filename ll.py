5b26dea6d08e1a3f84aa504c52157b19bff97577
qqsharki4 <159995328+qqsharki4@users.noreply.github.com>
2025-12-08 03:21:05 +0700

    Update ll.py

--- ll.py
+++ ll.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.фыв
 
-# meta developer: @qq_shark
+# meta developer: @хуярк типа
 
 __version__ = (1, 0, 1)
 
